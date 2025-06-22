f6dc400dcf498575b9959ca12c565186b4594830
Koh Mingyang <kohmingyang@outlook.com>
2025-06-22 14:59:55 +0800

    feat(logger): configured logger

--- app.py
+++
@@ -1,6 +0,0 @@
-def main():
-    raise NotImplementedError("This function is not implemented yet.")
-
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from eduvisor!")
+from services.logger import Logger
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+log = Logger()
+
+
+@app.get("/")
+async def read_root():
+    await log.info("Root endpoint accessed")
+    return {"message": "Welcome to the Eduvisor API"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app)
