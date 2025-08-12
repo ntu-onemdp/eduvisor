6aa1d72c8487b25dc2bc11fff2fa4f13f8ec723b
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2025-08-12 10:06:16 +0800

    fix: service acc key path to /secrets

--- models/vector_store.py
+++ models/vector_store.py
@@ -23,7 +23,7 @@ env = os.getenv("ENV")
 if env == "DEV":
     path = "credentials/service-account-key.json"
 else:
-    path = "/mnt/secrets/service-account-key"
+    path = "/secrets/service-account-key"
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
 
